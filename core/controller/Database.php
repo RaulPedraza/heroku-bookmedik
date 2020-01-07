d707e9ff2b50d4c75a7b463ca99fd2e0a58790ad
Raul <raul.pedraza@ikasle.egibide.org>
2020-01-07 12:51:47 +0100

    conexion

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,7 +3,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="bookmedik";
+		$this->user="b9d075f51b9f19";$this->pass="3dec4c23";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_4b41cf52284f1e3";
 	}
 
 	function connect(){
